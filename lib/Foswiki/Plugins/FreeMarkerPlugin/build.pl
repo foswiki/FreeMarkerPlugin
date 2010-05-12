23d9df056f9c2e797c6a30da01646a1d896edb92
ArthurClemens <ArthurClemens@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-05-12 21:29:31 +0000

    Item9014: use correct build package
    git-svn-id: http://svn.foswiki.org/trunk/FreeMarkerPlugin@7385 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/FreeMarkerPlugin/build.pl
+++ lib/Foswiki/Plugins/FreeMarkerPlugin/build.pl
@@ -11,7 +11,7 @@ BEGIN {
 use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::Plugins::Build('FreeMarkerPlugin');
+$build = new Foswiki::Contrib::Build('FreeMarkerPlugin');
 
 # Build the target on the command line, or the default target
 $build->build( $build->{target} );
